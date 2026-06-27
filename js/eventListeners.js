09a006772494a0d242b0d060ead34117132726d3
Yasin Fathollahi <yasin.fathollahi82@gmail.com>
2026-06-27 17:13:04 +0330

    Build search result card component, searching movies and showing results first version works fine

--- js/eventListeners.js
+++ js/eventListeners.js
@@ -1,20 +1,41 @@
 import { searchMovieByTitle } from './data.js';
-const searchResults = document.querySelector('.search-results');
+import { formImageURL } from './util.js';
+import resultCard from './components/resultCard.js';
+const searchResultsList = document.querySelector('.results-list');
 const searchForm = document.querySelector('.search-form');
 
 function renderSearchResults(results) {
-  results.forEach((result) =>
-    searchResults.insertAdjacentHTML(
-      'beforeend',
-      `<li class="result-card">${result.title}</li>`,
-    ),
-  );
+  searchResultsList.textContent = '';
+  results.forEach((result) => {
+    const poster = formImageURL(result.poster_path, 'poster', 'mobile');
+    const year = new Date(result.release_date).getFullYear();
+
+    // TODO: also try the new Temporal API
+
+    const movie = {
+      title: result.title,
+      poster,
+      year,
+      voteAverage: result.vote_average,
+      voteCount: result.vote_count,
+    };
+
+    searchResultsList.insertAdjacentHTML('beforeend', resultCard(movie));
+  });
 }
+
+window.addEventListener('load', () => {});
 
 searchForm.addEventListener('submit', async (event) => {
   event.preventDefault();
   const formData = new FormData(searchForm);
   const movieName = Object.fromEntries(formData.entries())['movie-name'];
   const { results } = await searchMovieByTitle(movieName, 1);
+
+  const searchTitle = (document.querySelector('.search-title').textContent =
+    movieName);
+
+  console.log(results);
+
   renderSearchResults(results);
 });
