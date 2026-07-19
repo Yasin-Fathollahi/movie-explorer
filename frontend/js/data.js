6cbbf2a3fa6b466cb141169e1a664bf4d81cb4dd
Yasin Fathollahi <yasin.fathollahi82@gmail.com>
2026-07-19 19:45:37 +0330

    minor path fix

--- frontend/js/data.js
+++ frontend/js/data.js
@@ -1,4 +1,4 @@
-import { API_BASE_URL } from './config';
+import { API_BASE_URL } from './config.js';
 
 export async function search(query, filters, page = 1) {
   const url = new URL('api/search', API_BASE_URL);
