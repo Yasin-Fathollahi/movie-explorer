6cf2f35de67982d80f7446023eca5d9adac5815e
Yasin Fathollahi <yasin.fathollahi82@gmail.com>
2026-07-19 19:34:28 +0330

    minor fix

--- frontend/js/data.js
+++ frontend/js/data.js
@@ -1,7 +1,7 @@
 import { API_BASE_URL } from './config';
 
 export async function search(query, filters, page = 1) {
-  const url = new URL('/api/search', API_BASE_URL);
+  const url = new URL('api/search', API_BASE_URL);
   url.searchParams.set('query', query);
   filters.forEach((filter) => url.searchParams.append('filter', filter));
   url.searchParams.set('page', page);
